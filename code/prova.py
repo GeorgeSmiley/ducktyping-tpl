74aba43236ae5c3c2fd124adf2685aabf70a19ab
GeorgeSmiley <tommasopuccetti@gmail.com>
2019-04-19 14:48:02 +0200

    updated

--- code/prova.py
+++ code/prova.py
@@ -1,5 +1,11 @@
 
 
-print(2+2)
+if False:
+	print(10+"ten") #This branch never execute, so the type checking ignore the type inconguency.
+else:
+	print(10+10)
+
+print(10+"ten") #Once executed the type check raise a type error.
+
 
 
